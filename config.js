b29ee23b0571fed075d40e78af8539c6870925ee
ednielson <niels@freelanceit.be>
2023-09-25 20:26:32 +0200

    change support mail

--- config.js
+++ config.js
@@ -33,9 +33,9 @@ const config = {
     // REQUIRED — Email 'From' field to be used when sending magic login links
     fromNoReply: `QRart <noreply@mail.qrart.ai>`,
     // REQUIRED — Email 'From' field to be used when sending other emails, like abandoned carts, updates etc..
-    fromAdmin: `QRart <hello@mail.qrart.ai>`,
+    fromAdmin: `QRart <hello@qrart.ai>`,
     // Email shown to customer if need support. Leave empty if not needed => if empty, set up Crisp above, otherwise you won't be able to offer customer support."
-    supportEmail: 'QRart <hello@mail.qrart.ai>',
+    supportEmail: 'QRart <hello@qrart.ai>',
     // When someone replies to supportEmail sent by the app, forward it to the email below (otherwise it's lost). If you set supportEmail to empty, this will be ignored.
     forwardRepliesTo: "niels@freelanceit.be",
   },
